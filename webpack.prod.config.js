67632043177551b7d7a0084230cfaac2754d34bf
Wilson Collado <colladow@gmail.com>
2020-05-07 23:37:32 -0400

    Changing build to create /docs directory for gh pages.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.js',
 
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './docs'),
     filename: 'index.bundle.js',
   },
 
